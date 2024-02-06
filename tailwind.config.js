d81881235aa6610941111c100c7695555d770d9d
keiner <keinermendoza@gmail.com>
2024-02-05 20:34:53 -0400

    fix including tailwind file configuration

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/templates/**/*.html", "./**/static/**/*.js", "./templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

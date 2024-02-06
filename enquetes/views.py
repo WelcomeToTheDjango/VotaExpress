57a494c8bd9b66525ce379a8eeb521ec8c9c8d37
keiner <keinermendoza@gmail.com>
2024-02-05 20:20:50 -0400

    adding tailwindcss
    create folder tailwind and add it to the STATICFILES_DIRS in settings
    create base.html in enquetes for use as base template
    create view home for render home.html in http://127.0.0.1:8000/enquetes/

--- enquetes/views.py
+++ enquetes/views.py
@@ -1,7 +1,9 @@
 # This file is used to handle the requests of the enquetes app
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
+def home(request):
+    return render(request, "enquetes/home.html")
 
 def index(request):
     return HttpResponse("Hello World")

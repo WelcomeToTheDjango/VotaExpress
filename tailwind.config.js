682a04949bb5a33673fd25e6b7a6dfc2cc68149e
Carlos L. ✔ <97265556+LopesLs@users.noreply.github.com>
2024-02-06 22:01:09 -0300

    refactor(configs): Removing unused content searching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/templates/**/*.html", "./**/static/**/*.js", "./templates/**/*.html"],
+  content: ["./**/templates/**/*.html", "./**/static/**/*.js"],
   theme: {
     extend: {},
   },

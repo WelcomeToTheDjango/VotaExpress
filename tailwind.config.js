ea7b90d01eb7e18e59960c949f5309f2cc87aa65
Carlos L. ✔ <97265556+LopesLs@users.noreply.github.com>
2024-02-10 19:46:11 -0300

    fix(color): Implement right background color from prototype

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/templates/**/*.html", "./**/static/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        'custom-color': '#F0EAEA',
+      },
       spacing: {
         '0-15em': '0.15em',
       },

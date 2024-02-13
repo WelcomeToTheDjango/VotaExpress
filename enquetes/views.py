680d24f6c6c8c0191b0734dcf9a355b1d6268694
Carlos L. ✔ <97265556+LopesLs@users.noreply.github.com>
2024-02-13 12:09:19 -0300

    feat(views): Implement login_required decorator
    - With this we can block all users of access the home view without authenticated first.

--- enquetes/views.py
+++ enquetes/views.py
@@ -1,9 +1,10 @@
 # This file is used to handle the requests of the enquetes app
-from django.shortcuts import render
-from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.conf import settings
+
+
+@login_required(login_url=settings.LOGIN_URL)
 def home(request):
     return render(request, "enquetes/home.html")
-
-def index(request):
-    return HttpResponse("Hello World")
